1bafbea3aef9d08dcbfbc73c009cc52e072c4a3d
shivam <shivamsoni.soni3@gmail.com>
2020-04-13 13:25:47 +0530

    Add: Functionality in java
    The app is fully functional now.
    Grading for Questions with checkbox has implemented.
    Final score is displayed on the bottom .
    the correct answers background is shown in white .

--- app/src/main/java/com/example/quiz_app/MainActivity.java
+++ app/src/main/java/com/example/quiz_app/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -42,20 +43,25 @@ public class MainActivity extends AppCompatActivity {
         int rg_id_2 = R.id.rg_no_2;
         int rg_id_3 = R.id.rg_no_3;
 
+        RadioButton rb_q1 = findViewById(R.id.rdb_q1_1);
+        RadioButton rb_q2 = findViewById(R.id.rdb_q2_3);
+        RadioButton rb_q3 = findViewById(R.id.rdb_q3_2);
         radioGroup = findViewById(rg_id_1);
         int selectedId_1 = radioGroup.getCheckedRadioButtonId();
         if (selectedId_1 != -1) {
             RadioButton reponse_1 = findViewById(selectedId_1);
             if (reponse_1.getText().toString().equalsIgnoreCase(Answers[0])) {
                 score++;
+                rb_q1.setBackgroundColor(getResources().getColor(R.color.white));
             }
         }
         radioGroup = findViewById(rg_id_2);
         int selectedId_2 = radioGroup.getCheckedRadioButtonId();
         if (selectedId_2 != -1) {
             RadioButton reponse_2 = findViewById(selectedId_2);
-            if (reponse_2.getText().toString().equalsIgnoreCase(Answers[0])) {
+            if (reponse_2.getText().toString().equalsIgnoreCase(Answers[1])) {
                 score++;
+                rb_q2.setBackgroundColor(getResources().getColor(R.color.white));
             }
         }
 
@@ -63,14 +69,63 @@ public class MainActivity extends AppCompatActivity {
         int selectedId_3 = radioGroup.getCheckedRadioButtonId();
         if (selectedId_3 != -1) {
             RadioButton reponse_3 = findViewById(selectedId_3);
-            if (reponse_3.getText().toString().equalsIgnoreCase(Answers[0])) {
+            if (reponse_3.getText().toString().equalsIgnoreCase(Answers[2])) {
                 score++;
+                rb_q3.setBackgroundColor(getResources().getColor(R.color.white));
             }
         }
 
 
     }
 
+
+    void check_for_cb() {
+        //correct answers for q 4 are 1 & 2
+        CheckBox Op_4q_1 = findViewById(R.id.cb_q4_1);
+        CheckBox Op_4q_2 = findViewById(R.id.cb_q4_2);
+        CheckBox Op_4q_3 = findViewById(R.id.cb_q4_3);
+        CheckBox Op_4q_4 = findViewById(R.id.cb_q4_4);
+        boolean flag = false;
+        if (Op_4q_1.isChecked() || Op_4q_2.isChecked() || Op_4q_3.isChecked() || Op_4q_4.isChecked()) {
+            flag = true;
+
+        }
+
+        if (flag) {
+
+            if (Op_4q_3.isChecked() || Op_4q_4.isChecked()) {
+                //condition for false answer
+                Log.w("V", "4 incorrect answer ");
+            } else {
+                Log.w("V", "4 correct answer ");
+                score++;
+                Op_4q_1.setBackgroundColor(getResources().getColor(R.color.white));
+                Op_4q_2.setBackgroundColor(getResources().getColor(R.color.white));
+            }
+        }
+
+        //correct answers for q 5 are 1 & 4
+        CheckBox Op_5q_1 = findViewById(R.id.cb_q5_1);
+        CheckBox Op_5q_2 = findViewById(R.id.cb_q5_2);
+        CheckBox Op_5q_3 = findViewById(R.id.cb_q5_3);
+        CheckBox Op_5q_4 = findViewById(R.id.cb_q5_4);
+        boolean flag1 = false;
+        if (Op_5q_1.isChecked() || Op_5q_2.isChecked() || Op_5q_3.isChecked() || Op_5q_4.isChecked()) {
+            flag1 = true;
+        }
+
+        if (flag1) {
+            if (Op_5q_2.isChecked() || Op_5q_3.isChecked()) {
+                //condition for false answer
+                Log.w("V", "5 incorrect answer ");
+            } else {
+                Log.w("V", "5 correct answer ");
+                score++;
+                Op_5q_1.setBackgroundColor(getResources().getColor(R.color.white));
+                Op_5q_4.setBackgroundColor(getResources().getColor(R.color.white));
+            }
+        }
+    }
 
     void details_Response() {
         EditText ET_name = findViewById(R.id.ET_name);
@@ -93,11 +148,10 @@ public class MainActivity extends AppCompatActivity {
         String age = ET_age.getText().toString();
         if ((TextUtils.isEmpty(age)) || ((selectedId == -1)) || (TextUtils.isEmpty(name))) {
             Log.w("V", "no name ");
-            Toast msg = Toast.makeText(getApplicationContext(), "Please fill all the details  ", Toast.LENGTH_LONG);
+            Toast msg = Toast.makeText(getApplicationContext(), getString(R.string.Please_fill), Toast.LENGTH_LONG);
             msg.show();
             flag = false;
         }
-
 
         return flag;
     }
@@ -108,7 +162,10 @@ public class MainActivity extends AppCompatActivity {
         Final_TV.setText(getString(R.string.Text_Name));
         Final_TV.append(name + "\n" + getString(R.string.Text_Age) + age);
         Final_TV.append("\n" + getString(R.string.Text_Gender) + gender);
-        Final_TV.append("\n" + " The Score is " + score);
+        Final_TV.append("\n" + getString(R.string.Score_is) + " " + score + " /5");
+
+
+
     }
 
     public void On_Submit(View view) {
@@ -117,6 +174,7 @@ public class MainActivity extends AppCompatActivity {
             if (check_Details(flag)) {
 
                 check_for_rad();
+                check_for_cb();
                 set_Results();
 
             }
